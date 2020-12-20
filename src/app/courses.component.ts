06c78e429837cd789109ca1518e4da1e9b9064a0
aaron dalao <aaron.dalao@gmail.com>
2020-12-21 07:06:10 +0800

    used directives in this practice. added a courses added a course arraycourses array to simulate a list of courses and tried to render it in a directive with interpolated course. also used tilde  so i can make a new line

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -2,10 +2,19 @@ import{ Component } from '@angular/core';
 
 @Component({
     selector: 'courses', // basic CSS selector: <courses> "courses" OR <div class="courses"> ".courses" OR <div id="courses"> "#courses"
-    template: '<h2>{{ getTitle() }}</h2>'
+    template: `
+
+        <h2>{{ getTitle() }}</h2>
+        <ul>
+            <li *ngFor='let course of courses'> 
+                {{ course }}
+            </li>
+        </ul>
+    `
 })
 export class CoursesComponent {
     title = 'List of Courses';
+    courses = ["courses1", "courses2", "courses3"];
 
     getTitle() {
         return this.title;
