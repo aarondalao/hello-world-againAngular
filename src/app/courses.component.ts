e0721ae50ef691c22cf7b0a40b7a402e252415f9
aaron dalao <aaron.dalao@gmail.com>
2020-12-21 05:10:08 +0800

    first initial working commit. PLEASE BRANCH OUT FROM THIS

---
+++ src/app/courses.component.ts
@@ -0,0 +1,8 @@
+import{ Component } from '@angular/core';
+
+@Component({
+    selector: 'courses', // basic CSS selector: <courses> "courses" OR <div class="courses"> ".courses" OR <div id="courses"> "#courses"
+    template: '<h2>Courses</h2>'
+})
+export class CoursesComponent {
+}
